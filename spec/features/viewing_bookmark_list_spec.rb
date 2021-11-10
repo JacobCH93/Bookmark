51147bd9e050da515b998c5fc35ed06faf3d4c8b
Mark McLeod <markmcleod45@gmail.com>
2021-11-10 16:39:22 +0000

    Rename feature test files. Update viewing_bookmark_spec.rb test

--- spec/features/viewing_bookmark_list_spec.rb
+++
@@ -1,8 +0,0 @@
-feature 'Viewing bookmarks list' do
-    scenario 'visiting the index page returns a list of bookmarks' do
-      visit('/bookmarks')
-      expect(page).to have_content "http://www.makersacademy.com"
-      expect(page).to have_content "http://www.google.com"
-      expect(page).to have_content "http://www.destroyallsoftware.com"
-    end
-  end
